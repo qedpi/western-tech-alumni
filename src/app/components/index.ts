b389f22c66e9e70157ce68d609b81d41f6e3a858
= <junruihu@gmail.com>
2020-11-29 21:42:51 -0500

    make appbar

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,3 +4,4 @@ export { TodoList } from './TodoList';
 export { TodoItem } from './TodoItem';
 export { TodoTextInput } from './TodoTextInput';
 export { StudentList } from './StudentList';
+export { MyAppBar } from './MyAppBar'
